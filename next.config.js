db0ba35568504344012096df019917d72ae134dd
Lumiho <zavalaleo715@yahoo.com>
2025-05-22 01:37:08 -0700

    Fixed lingering issues with build

--- next.config.js
+++ next.config.js
@@ -1,15 +1,33 @@
-const createMDX = require('@next/mdx')
-
-const withMDX = createMDX({
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
     rehypePlugins: [],
+    providerImportSource: "@mdx-js/react",
   },
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    mdxRs: true,
+  },
+  // Configure webpack to handle MDX files
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: '@mdx-js/loader',
+          options: {
+            providerImportSource: "@mdx-js/react",
+          },
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = withMDX(nextConfig) 
